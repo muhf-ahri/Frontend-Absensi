b4b4c059c825c367839509d17fbdcfff7d651529
muhf-ahri <fahrimuhammadani123@gmail.com>
2025-10-19 21:02:41 +0700

    Frontend Repo Edit API2

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'backend-absensi-production.up.railway.app';
+const API_BASE_URL = 'https://backend-absensi-production.up.railway.app/api';
 
 // Create axios instance
 const api = axios.create({
